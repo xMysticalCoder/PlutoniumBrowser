efc79a823894ddad881a065be874276d74704010
xMystical Coder <74260385+xMysticalCoder@users.noreply.github.com>
2022-05-02 21:00:17 +0000

    Fixed website creation (website creation not done, waiting for css)

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 
 import random, string
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from replit import db
 f = Flask(  # Create a flask app
 	__name__,
 	template_folder='templates',  # Name of html file folder
 	static_folder='static'  # Name of directory for static files
 )
+
 db["google.com"] = "testing lol 123"
+del db["google.com"]
 ok_chars = string.ascii_letters + string.digits
 
 @f.route('/')
@@ -17,12 +19,41 @@ def home():
 
 @f.route('/search', methods=['GET', 'POST'])
 def search():
-  website = request.form.get('url')
-  if website in db.keys():
-    data = db[website]
+  if request.method == "POST":
+    
+  
+    website = request.form.get('url')
+    if website in db.keys():
+      data = db[website]
+      print("Website")
+    else:
+      data = "404 Website Not Found"
+    return render_template('search.html', data=data, website=website)
   else:
-    data = "404 Website Not Found"
-  return render_template('search.html', data=data, website=website)
+    return render_template("home.html")
+
+@f.route("/createweb", methods=['GET', 'POST'])
+def createWeb():
+  return render_template("createweb.html")
+
+
+@f.route("/createwebverify", methods=['GET', 'POST'])
+def createWebVerify():
+  if request.method == "POST":
+    createwebname = request.form.get("createwebname")
+    createwebdata = request.form.get("createwebdata")
+    if createwebname.endswith(".com") and createwebname not in db.keys():
+      return render_template("createwebsuccess.html", website = createwebname)
+      
+    else:
+      if createwebname in db.keys():
+        return render_template("createwebfailed2.html")
+      else:
+        return render_template("createwebfailed1.html")
+  else:
+    return render_template("createweb.html")
+
+
 
 if __name__ == "__main__":  # Makes sure this is the main process
 	f.run( # Starts the site
